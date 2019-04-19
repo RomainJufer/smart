e64cf7eaa13a70bd9c01e1fb24075fa79d991f2e
RomainJufer <romain.jufer@epfl.ch>
2019-04-19 13:33:51 +0200

    Add helper function to check if fd is a constructor

--- core/src/main/scala/stainless/extraction/smartcontracts/Trees.scala
+++ core/src/main/scala/stainless/extraction/smartcontracts/Trees.scala
@@ -40,6 +40,8 @@ trait Trees extends methods.Trees { self =>
       }
     }
 
+    def isConstructor(implicit symbols: self.Symbols): Boolean = fd.isContractMethod && fd.id.name == "constructor"
+
     def isInvariant(implicit symbols: self.Symbols): Boolean = fd.isInSmartContract && fd.id.name == "invariant"
     def isContractMethod(implicit symbols: self.Symbols): Boolean = !fd.isInvariant && fd.isInSmartContract && !fd.isAccessor
   }
